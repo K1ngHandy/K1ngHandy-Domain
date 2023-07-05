46be7dc6862e1ff8cd2b6879db85ad05efb0df59
K1ngHandy <a.king.life@pm.me>
2023-07-05 14:29:09 -0400

    call to database function

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ require('dotenv').config();
 const { Client } = require("@notionhq/client");
 // Initialize notion client
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+// Import database ID
+const databaseId = process.env.NOTION_API_DATABASE;
+
+// makes call to database
+const getDatabase = async () => {
+    const response = await notion.databases.query({ database_id: databaseId });
+
+    console.log(response);
+};
+
+getDatabase();
