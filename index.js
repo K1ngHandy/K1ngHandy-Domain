c41cdfe9f0b31ddb8427de90209ae119babe1b46
K1ngHandy <a.king.life@pm.me>
2023-07-06 23:17:45 -0400

    update async, need correct ID

--- index.js
+++ index.js
@@ -9,10 +9,19 @@ const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_API_DATABASE;
 
 // makes call to database
-const getDatabase = async () => {
+exports.getDatabase = async () => {
     const response = await notion.databases.query({ database_id: databaseId });
 
-    console.log(response);
+    const responseResults = response.results.map((page) => {
+        return {
+            id: page.id,
+            name: page.properties.Name.title[0]?.plain_text,
+            role: page.properties.Role.rich_text[0]?.plain_text,
+        };
+    });
+
+    // log results
+    console.log(responseResults);
 };
 
 getDatabase();
